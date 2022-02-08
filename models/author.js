069563f8c69cea8ca64d5edef30804a94c649f0b
Andrew Chen <shiki510520.com>
2022-02-09 01:37:08 +0900

    Finished Author Routes

--- models/author.js
+++ models/author.js
@@ -1,11 +1,23 @@
 const mongoose = require('mongoose')
-const { stringify } = require('nodemon/lib/utils')
+const Book = require('./book')
 
 const AuthorSchema = new mongoose.Schema({
     name:{
         type: String,
         required: true
     }
+})
+
+AuthorSchema.pre('remove', function(next){
+    Book.find({author:this.id}, (err, books) => {
+        if (err) {
+            next(err)
+        } else if (books.length > 0){
+            next(new Error('This author has books still'))
+        } else {
+            next()
+        }
+    })
 })
 
 module.exports = mongoose.model('Author', AuthorSchema)
